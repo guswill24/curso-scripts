e7925b918693a90f3c584f20d5bda07e770a425c
Gustavo Sánchez Rodríguez <125821428+guswill24@users.noreply.github.com>
2025-04-01 15:13:09 -0500

    Update index.js

--- jenkins-tests/test/index.js
+++ jenkins-tests/test/index.js
@@ -1,14 +1,20 @@
-const sum = require('../');
-const assert = require('assert');
+const { expect } = require('chai');
+const { sumar } = require('../index');
 
-describe('Sum', () => {
-  it('should handle positive numbers', () => {
-    assert.equal(sum(3, 5), 8);
+describe('Función sumar', () => {
+  it('debería sumar dos números positivos', () => {
+    expect(sumar(3, 5)).to.equal(8);
   });
-  it('should handle negative numbers', () => {
-    assert.equal(sum(3, -5), -2);
+
+  it('debería sumar un número positivo y uno negativo', () => {
+    expect(sumar(3, -5)).to.equal(-2);
   });
-  it('should handle decimals', () => {
-    assert.equal(sum(3.5, 7.1), 10.6);
+
+  it('debería sumar números decimales', () => {
+    expect(sumar(3.5, 7.1)).to.equal(10.6);
+  });
+
+  it('debería lanzar un error si los parámetros no son números', () => {
+    expect(() => sumar('3', 5)).to.throw('Ambos parámetros deben ser números');
   });
 });
